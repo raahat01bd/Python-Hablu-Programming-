b4a7ee9e01b2df92b1adc50ba59427bc740498d1
raahat01bd <merahatbd@gmail.com>
2025-10-25 12:38:30 +0600

    Added my new work

---
+++ First Project/data_type.py
@@ -0,0 +1,25 @@
+#Data types in Python
+
+a = 10
+b = 3.50
+c = "Hello"
+d = True
+e = [1, 2, 3]
+f = (5, 6, 7)
+g = {8, 9, 10}
+h = {"name": "Rahat"}
+i = None
+
+#Check Data types
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+
+numbers = range(4)
+print(numbers)
---
+++ First Project/define_variable.py
@@ -0,0 +1,18 @@
+#Variavle define in python
+
+name = "Rahat"
+age = 22
+height = 5.6
+is_student = True
+fruits = ["Apple", "Orange", "Mango"]
+student = {"name": "Rahat", "age": 22}
+
+#Cheacking Variable Type
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(fruits))
+print(type(student))
+
