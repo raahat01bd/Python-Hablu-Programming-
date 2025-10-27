2d647298fed066471fb9b58806809caee20cb852
raahat01bd <merahatbd@gmail.com>
2025-10-27 22:29:21 +0600

    Add new work

---
+++ First Project/problem1.py
@@ -0,0 +1,29 @@
+#print a poem/rythm
+
+print('''Twinkle twinkle little star.
+How I wonder what you are.
+Up above the world so high.
+Like a diamond in the sky.
+Twinkle twinkle little star.
+How I wonder what you are.
+
+Twinkle twinkle little star.
+How I wonder what you are.
+Up above the world so high.
+Like a diamond in the sky.
+Twinkle twinkle little star.
+How I wonder what you are.''')
+
+#use REPL and print the table of 5
+
+print(5*1)
+print(5*2)
+print(5*3)
+print(5*4)
+print(5*5)
+print(5*6)
+print(5*7)
+print(5*8)
+print(5*9)
+print(5*10)
+
